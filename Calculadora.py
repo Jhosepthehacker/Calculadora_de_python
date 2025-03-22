2b9d4cd6026538e45dcfa00f0989aac090b5806d
Jhosepthehacker <josemanuelramirezrengifo9@gmail.com>
2025-03-22 13:33:38 -0400

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,11 @@
+#python 3.12
+
+#Entrada de datos del usuario 
+
+a = int(input("Ingrese un número para calcular"))
+print ("")
+b = int(input("Ingrese el segundo número para sumar"))
+c = a + b
+#Calculando
+
+print (f"{a} + {b} = " + str(c))
