24cfa3a46abfc69c83b905e0db13329b044e3656
Jhosepthehacker <josemanuelramirezrengifo9@gmail.com>
2025-03-26 11:14:58 -0400

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -2,9 +2,9 @@
 
 #Entrada de datos del usuario 
 
-a = int(input("Ingrese un número para calcular"))
+a = int(input("Ingrese un número para calcular: "))
 print ("")
-b = int(input("Ingrese el segundo número para sumar"))
+b = int(input("Ingrese el segundo número para sumar: "))
 c = a + b
 #Calculando
 
