495b4572de7779de45ee145a6cc0f01cc57ce36f
Jhosepthehacker <josemanuelramirezrengifo9@gmail.com>
2025-03-28 22:01:47 -0400

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,11 +1,12 @@
 #python 3.12
 
 #Entrada de datos del usuario 
+if __name__ == '__main__':
+  
+    a = int(input("Ingrese un número para calcular: "))
+    print ("")
+    b = int(input("Ingrese el segundo número para sumar: "))
+    c = a + b
+  # Calculando y mostrando el resultado
 
-a = int(input("Ingrese un número para calcular: "))
-print ("")
-b = int(input("Ingrese el segundo número para sumar: "))
-c = a + b
-#Calculando
-
-print (f"{a} + {b} = " + str(c))
+    print (f"{a} + {b} = " + str(c))
