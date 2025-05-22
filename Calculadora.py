424b5bf880374647b984d68539209883dffa9d1e
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-05-22 19:42:45 -0400

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,18 +1,12 @@
 #python 3.12
 
 #Entrada de datos del usuario 
-if __name__ == '__main__':
-  
     a = int(input("Ingrese un número para calcular: "))
-    print ("")
+    print("")
     b = int(input("Ingrese el segundo número para sumar: "))
-    c = a + b
+    c = (f"{a} + {b}")
   # Calculando y mostrando el resultado
-
-    print (f"{a} + {b} = " + str(c))
-
-    print ("Espero que hayas disfrutado de la calculadora")
-  
-    print ("")
-
-    print ("En desarrollo....")
+    print(f"El resultado es: {c}")
+    print("Espero que hayas disfrutado de la calculadora")
+    print("")
+    print("En desarrollo....")
