9e2a32b0187cbc51f79357d53d2ed18c4c1e1dd7
DarkCode <josemanuelramirezrengifo9@gmail.com>
2025-05-22 20:08:47 -0400

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,12 +1,32 @@
 #python 3.12
 
 #Entrada de datos del usuario 
-    a = int(input("Ingrese un número para calcular: "))
-    print("")
-    b = int(input("Ingrese el segundo número para sumar: "))
-    c = (f"{a} + {b}")
+def calculadora():
+    try:
+        a = int(input("Ingrese un número para calcular: "))
+        print("")
+        b = int(input("Elije el operable: suma, resta, multi, div"))
+        c = int(input("Ingrese el segundo número para calcular: "))
+    except ValueError:
+        print("¡Inválido! por favor solo ingrese números y el operador dependiendo de lo que te diga :)")
+  # Comprobando la respuesta del usuario
+    if b.lower() == "suma":
+        resultado_suma = (f"{a} + {c}")
+        print(f"El resultado es: {resultado_suma}")
+    elif b.lower() == "resta":
+        resultado_resta = (f"{a} + {c}")
+        print(f"{resultado_resta}")
+    elif b.lower() == "multi":
+        resultado_multi = (f"{a} * {c}")
+    elif b.lower() == "div":
+        resultado_div = (f"{a} / {c}")
+        print(f"{resultado_div}")
+    else:
+        print("¡Entrada Inválida! escriba por favor números y operadores ")
   # Calculando y mostrando el resultado
-    print(f"El resultado es: {c}")
-    print("Espero que hayas disfrutado de la calculadora")
+    print(f"El resultado es: {d}")
+    print("Espero que hayas disfrutado de la calculadora ^_^")
     print("")
     print("En desarrollo....")
+if __name__ == '__main__':
+    calculadora()
